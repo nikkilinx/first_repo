930bde316f3b1dba39bdb816b6bd97ba770ff816
nikkilinx <nicole.l.lincoln@gmail.com>
2019-01-06 21:10:00 -0700

    Is this working

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(){
 
     cout<<"Hello World! Today is a good day!!"<<endl;
-
+    cout<<"What day is it?"<<endl;
+    day_of_the_week{};
+    cin>>day_of_the_week;
+    cout<<day_of_the_week<<"is a good day"<<endl;
     return 0;
 }
