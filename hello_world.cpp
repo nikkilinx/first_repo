010f82d80d9c4d5be58468d696e54751d6bcac79
nikkilinx <nicole.l.lincoln@gmail.com>
2019-01-09 21:05:48 -0700

    added additional text

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,9 +6,12 @@ int main(){
 
     cout<<"Hello World! Today is a good day!!"<<endl;
     cout<<"What day is it?"<<endl;
-    day_of_the_week{};
+    string day_of_the_week{};
     cin>>day_of_the_week;
     cout<<day_of_the_week<<"is a good day!"<<endl;
-    cout<<"You are a badass!"
+    cout<<"You are a badass!";
+    cout<<"What is on the agenda today?"<<endl;
+    cin.get(agenda, 100);
+    cout<<agenda<<" sounds like fun."<<endl;
     return 0;
 }
